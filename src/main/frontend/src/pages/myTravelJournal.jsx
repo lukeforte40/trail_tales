8789761d490cca83b3f518042c34ee9c28ddacd0
luke <lukeforte40@gmail.com>
2025-05-13 14:36:29 -0400

    added tool tip

--- src/main/frontend/src/pages/myTravelJournal.jsx
+++ src/main/frontend/src/pages/myTravelJournal.jsx
@@ -1,14 +1,18 @@
 import styles from "../styles/pages/myTravelJournal.module.scss"
 import { GoPlus } from "react-icons/go";
+import { Tooltip } from 'react-tooltip'
 
 export default function Jorunal(){
     return(
         <div id={styles.journalContainers}>
             <h1 id={styles.TripTitle}>Your Trips</h1>
             <div className={styles.tripsContainer}>
-                <div className={styles.tripContainer}>
+                <Tooltip anchorSelect="#startTrip" place="top" id={styles.toolTip}>
+                    Add a trip
+                </Tooltip>
+                <a className={styles.tripContainer} id="startTrip">
                     <GoPlus id={styles.plus}/>
-                </div>
+                </a>
             </div>
         </div>
     )
